0f706670d72267800440e9fb4cfa9b2a9a0f1f75
Taylor Sarrafian <surprisetalk@gmail.com>
2017-02-05 20:49:35 -0800

    gobby

--- gobby.rkt
+++ gobby.rkt
@@ -3,6 +3,43 @@
 (require net/http-client)
 (require html-parsing)
 
-(let-values (((a b c) (http-sendrecv "arademaker.github.io"
-                                     "/about.html")))
-  (html->xexp c))
+
+;; CLI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#|
+
+gobby <url> <destination>
+
+|#
+
+;;; TODO: command-line parsing
+
+
+;; CRAWLER ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; url-split : String -> Values (List String String)
+(define (url-split url)
+  (let ([paths (string-split url "/")])
+    (values (first paths)
+            (string-append "/"
+                           (string-join
+                            (rest paths)
+                            "/")))))
+
+;;; crawl : String -> XExp
+;;  eg (crawl "arademaker.github.io/about.html")
+(define (crawl url)
+  (let*-values ([(host endpoint) (url-split url)]
+                [(a b c) (http-sendrecv host endpoint)])
+    (html->xexp c)))
+
+
+;; GOBBY ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; gobby : String -> TODO
+(define (gobby src dest)
+  (cond [(url-match? src "youtube.com")
+         ;; TODO: without dest, we should use youtube-dl -o to pipe to stdout
+         (system (string-append "youtube-dl "
+                                src))]
+        [#t (crawl src)]))
